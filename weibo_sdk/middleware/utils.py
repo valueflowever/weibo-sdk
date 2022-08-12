c5e36371fcdd21590a15031022ae51eca24c495f
valueflowever <756162364@qq.com>
2022-08-12 16:38:56 +0800

    内容过滤

--- weibo_sdk/middleware/utils.py
+++ weibo_sdk/middleware/utils.py
@@ -1,10 +1,14 @@
+black_word = ['神秘嘉宾', '手把手教你', '开课', '给大家发一个福利', '讲师', '置顶', '原图']
+
+
 def filter_text(text_list: list):
     n_list = []
-    for i in text_list:
-        if ('【' and '】') in list(set(i)) or i.count('-') > 3:
-            break
-        elif '神秘嘉宾' in i:
-            break
-        else:
-            n_list.append(i)
+    for text in text_list:
+        for w in black_word:
+            if w in text:
+                return n_list
+        if ('【' and '】') in list(set(text)) or text.count('-') > 3:
+            return n_list
+        if len(text) > 1:
+            n_list.append(text)
     return n_list
