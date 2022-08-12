6c350c10a44c25a0eb9c2f5a2d3349bc8953dc2b
valueflowever <756162364@qq.com>
2022-08-12 17:06:22 +0800

    内容过滤

--- weibo_sdk/middleware/utils.py
+++ weibo_sdk/middleware/utils.py
@@ -7,7 +7,7 @@ def filter_text(text_list: list):
         for w in black_word:
             if w in text:
                 return n_list
-        if ('【' and '】') in list(set(text)) or text.count('-') > 3:
+        if ('【' and '】') in list(set(text)) or text.count('-') > 3 or text.count('#') == 2:
             return n_list
         if len(text) > 1:
             n_list.append(text)
